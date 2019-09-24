cc3810adf79cc18fc3481b3e30e5b477d34c48dc
mattguo5 <39818100+mattguo5@users.noreply.github.com>
2019-09-24 12:02:09 -0700

    Add files via upload

---
+++ Code/Implementation/PeoriaDemo/PeoriaDEMO.ino
@@ -0,0 +1,395 @@
+/*
+  Reading multiple RFID tags, simultaneously!
+  By: Nathan Seidle @ SparkFun Electronics
+  Date: October 3rd, 2016
+  https://github.com/sparkfun/Simultaneous_RFID_Tag_Reader
+
+  Constantly reads and outputs any tags heard
+
+  If using the Simultaneous RFID Tag Reader (SRTR) shield, make sure the serial slide
+  switch is in the 'SW-UART' position
+*/
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+
+#define DRY_EPC  0xAA
+
+#define MEDIUM_EPC  0xCC
+
+#define WET_EPC   0xEE
+
+#define FERTIGATE_TAG   0xFF
+
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1
+#define PIN            6
+#define RELAY_PIN      10
+#define FERTIGATE_PIN  4
+#define FERTIGATE_LED  11
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS      1
+
+//What is the Threshold for dry?
+#define THRESHOLD_DRY     15
+
+//What is the Threshold for wet?
+#define THRESHOLD_MEDIUM     10
+
+//What length is the desired EPC array?
+#define EPC_LENGTH    100
+
+//How much to flash white during time a disconnect?
+#define FLASHAMOUNT   10
+
+//RSSI VALUE FOR THE RELAY TO TURN ON/OFF
+#define RELAY_RSSI    -60
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
+// example for more information on possible values.
+Adafruit_NeoPixel pixel = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+int delayval = 500; // delay for half a second
+unsigned long startMillis;
+unsigned long currentMillis;
+bool prevRelayState;
+
+#include <SoftwareSerial.h> //Used for transmitting to the device
+
+SoftwareSerial softSerial(2, 3); //RX, TX
+
+#include "SparkFun_UHF_RFID_Reader.h" //Library for controlling the M6E Nano module
+RFID nano; //Create instance
+
+void setup()
+{
+
+  delay(500);
+  Serial.begin(115200);
+  while (!Serial); //Wait for the serial port to come online
+
+  if (setupNano(38400) == false) //Configure nano to run at 38400bps
+  {
+    Serial.println(F("Module failed to respond. Please check wiring."));
+
+    while (1); //Freeze!
+  }
+
+  nano.setRegion(REGION_NORTHAMERICA); //Set to North America
+
+  nano.setReadPower(1300); //5.00 dBm. Higher values may caues USB port to brown out
+  //Max Read TX Power is 27.00 dBm and may cause temperature-limit throttling
+
+  //Serial.println(F("Press a key to begin scanning for tags."));
+  //while (!Serial.available()); //Wait for user to send a character
+  //Serial.read(); //Throw away the user's character
+
+  //Set up Relay pin to turn on
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(FERTIGATE_PIN, INPUT_PULLUP);
+  pinMode(FERTIGATE_LED, OUTPUT);
+
+
+  nano.startReading(); //Begin scanning for tags
+}
+
+void loop()
+{
+
+   bool nfertigatePass = digitalRead(FERTIGATE_PIN);
+   //digitalWrite(RELAY_PIN, false);
+   
+   if(nfertigatePass){
+          
+      digitalWrite(FERTIGATE_LED, false);
+      }
+   else{
+          
+      digitalWrite(FERTIGATE_LED, true);
+      }
+
+  
+  if (nano.check() == true) //Check to see if any new data has come in from module
+  {
+    byte responseType = nano.parseResponse(); //Break response into tag ID, RSSI, frequency, and timestamp
+
+    if (responseType == RESPONSE_IS_KEEPALIVE)
+    {
+      Serial.println(F("Scanning"));
+     // digitalWrite(RELAY_PIN, false);
+    }
+    else if (responseType == RESPONSE_IS_TAGFOUND)
+    {
+      /****************************
+       * Get appropriate sections of msg array for necessary displayed information
+       * Information includes:
+       *      - RSSI
+       *      - Frequency
+       *      - Moisture Value
+       *      - EPC
+       *      - EPC Desired Moisture Code (EPC Header)
+       ***************************/
+      //
+
+      startMillis = millis();
+      
+      int rssi = nano.getTagRSSINew(); //Get the RSSI for this tag read
+      long freq = nano.getTagFreqNew(); //Get the frequency this tag was detected at
+      long moisture = nano.getMoistureData();
+      byte tagEPCBytes = nano.getTagEPCBytesNew(); //Get the number of bytes of EPC from response
+      byte EPCHeader = nano.getEPCHeader();
+      byte EPCFertigate = nano.getFertigateTag();
+
+      if(rssi <= RELAY_RSSI){
+
+        digitalWrite(RELAY_PIN, false);
+        prevRelayState = false;
+        
+        }
+
+      //Filter unwanted response; checks to see if frequency is appropriate
+      if(freq < 1000000 && nano.getAntennaeIDNew() == 17){
+
+         if(nfertigatePass){
+
+          if(EPCHeader == DRY_EPC && EPCFertigate != FERTIGATE_TAG){
+
+            if(moisture >= THRESHOLD_DRY && rssi >= RELAY_RSSI){
+
+                digitalWrite(RELAY_PIN, false);
+                prevRelayState = false;
+              
+              }
+              else{
+                
+                digitalWrite(RELAY_PIN, true);
+                prevRelayState = true;
+                
+                }
+            
+            
+            }
+            else if(EPCHeader == MEDIUM_EPC && EPCFertigate != FERTIGATE_TAG){
+              
+              if(moisture >= THRESHOLD_MEDIUM && rssi >= RELAY_RSSI){
+
+                digitalWrite(RELAY_PIN, false);
+                prevRelayState = false;
+              
+              }
+              else{
+                
+                digitalWrite(RELAY_PIN, true);
+                prevRelayState = true;
+                
+                }
+              
+              
+              
+              }
+              else if(EPCHeader == WET_EPC && EPCFertigate != FERTIGATE_TAG){
+                
+                if(moisture <= THRESHOLD_MEDIUM && rssi >= RELAY_RSSI){
+
+                digitalWrite(RELAY_PIN, false);
+                prevRelayState = false;
+              
+                }
+                 else{
+                
+                digitalWrite(RELAY_PIN, true);
+                prevRelayState = true;
+                
+                }
+                
+              }
+          
+          }
+          else{
+            
+            if(EPCHeader == DRY_EPC && EPCFertigate == FERTIGATE_TAG){
+
+            if(moisture >= THRESHOLD_DRY && rssi >= RELAY_RSSI){
+
+                digitalWrite(RELAY_PIN, false);
+                prevRelayState = false;
+              
+              }
+              else{
+                
+                digitalWrite(RELAY_PIN, true);
+                prevRelayState = true;
+                
+                }
+            
+            
+            }
+            else if(EPCHeader == MEDIUM_EPC && EPCFertigate == FERTIGATE_TAG){
+              
+              if(moisture >= THRESHOLD_MEDIUM && rssi >= RELAY_RSSI){
+
+                digitalWrite(RELAY_PIN, false);
+                prevRelayState = false;
+              
+              }
+              else{
+                
+                digitalWrite(RELAY_PIN, true);
+                prevRelayState = true;
+                
+                }
+              
+              
+              
+              }
+              else if(EPCHeader == WET_EPC && EPCFertigate == FERTIGATE_TAG){
+                
+                if(moisture <= THRESHOLD_MEDIUM && rssi >= RELAY_RSSI){
+
+                digitalWrite(RELAY_PIN, false);
+                prevRelayState = false;
+              
+                }
+                 else{
+                
+                digitalWrite(RELAY_PIN, true);
+                prevRelayState = true;
+                
+                }
+                
+              }
+            
+            
+            }
+
+        //Print RSSI Value from msg array
+        Serial.print(F(" rssi["));
+        Serial.print(rssi);
+        Serial.print(F("]"));
+
+        //Print Frequency Value from msg array
+        Serial.print(F(" freq["));
+        Serial.print(freq);
+        Serial.print(F("]"));
+
+        //Print Moisture Value from msg array
+        Serial.print(F(" Moisture["));
+        Serial.print(moisture);
+        Serial.print(F("]"));
+
+      //Print EPC bytes, this is a subsection of bytes from the response/msg array
+        Serial.print(F(" epc["));
+        for (byte x = 0 ; x < tagEPCBytes ; x++)
+        {
+         if (nano.msg[31 + x] < 0x10) Serial.print(F("0")); //Pretty print
+          Serial.print(nano.msg[37 + x], HEX);
+          Serial.print(F(" "));
+        }
+        Serial.print(F("]"));
+
+        //Print EPC_Header Value from msg array
+        Serial.print(F(" epc_Header["));
+        Serial.print(EPCHeader, HEX);
+        Serial.print(F(" "));
+
+        Serial.print(F("]"));
+
+        Serial.print(F(" epc_fertigate["));
+        Serial.print(EPCFertigate, HEX);
+        Serial.print(F(" "));
+        Serial.print(F("]"));
+
+        Serial.println();
+
+
+        
+      }
+        
+     
+    }
+    else if (responseType == ERROR_CORRUPT_RESPONSE)
+    {
+      Serial.println("Bad CRC");
+      digitalWrite(RELAY_PIN, false);
+      prevRelayState = false;
+    }
+    else
+    {
+      //Unknown response
+      Serial.println("Unknown error");
+      digitalWrite(RELAY_PIN, false);
+      prevRelayState = false;
+    }
+  }
+  else{
+
+     currentMillis = millis();
+     currentMillis = currentMillis - startMillis;
+     //Serial.println(currentMillis);
+
+     if(currentMillis > 50){
+      
+      digitalWrite(RELAY_PIN, false);
+      }
+//     else{
+//
+//      digitalWrite(RELAY_PIN, false);
+//      
+//      }
+    //digitalWrite(RELAY_PIN, false);
+    //Serial.println("wazzup");
+    
+    }
+}
+
+//Gracefully handles a reader that is already configured and already reading continuously
+//Because Stream does not have a .begin() we have to do this outside the library
+boolean setupNano(long baudRate)
+{
+  nano.begin(softSerial); //Tell the library to communicate over software serial port
+
+  //Test to see if we are already connected to a module
+  //This would be the case if the Arduino has been reprogrammed and the module has stayed powered
+  softSerial.begin(baudRate); //For this test, assume module is already at our desired baud rate
+  while(!softSerial); //Wait for port to open
+
+  //About 200ms from power on the module will send its firmware version at 115200. We need to ignore this.
+  while(softSerial.available()) softSerial.read();
+  
+  nano.getVersion();
+
+  if (nano.msg[0] == ERROR_WRONG_OPCODE_RESPONSE)
+  {
+    //This happens if the baud rate is correct but the module is doing a ccontinuous read
+    nano.stopReading();
+
+    Serial.println(F("Module continuously reading. Asking it to stop..."));
+
+    delay(1500);
+  }
+  else
+  {
+    //The module did not respond so assume it's just been powered on and communicating at 115200bps
+    softSerial.begin(115200); //Start software serial at 115200
+
+    nano.setBaud(baudRate); //Tell the module to go to the chosen baud rate. Ignore the response msg
+
+    softSerial.begin(baudRate); //Start the software serial port, this time at user's chosen baud rate
+  }
+
+  //Test the connection
+  nano.getVersion();
+  if (nano.msg[0] != ALL_GOOD) return (false); //Something is not right
+
+  //The M6E has these settings no matter what
+  nano.setTagProtocol(); //Set protocol to GEN2
+
+  nano.setAntennaPort(); //Set TX/RX antenna ports to 1
+
+  return (true); //We are ready to rock
+}
